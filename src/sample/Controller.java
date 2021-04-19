079aba9942c44e99fbfc6b7bc619f850fa4cd143
dominik122 <56114022+dominik122@users.noreply.github.com>
2021-04-19 16:19:39 +0200

    Stworzenie przycisku i obsługi akcji przycisnięcia

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,17 @@
 package sample;
 
+
+import javafx.scene.control.Button;
+import javafx.event.ActionEvent;
+
 public class Controller {
+    public Button btnMain;
+    int clickCounter = 0;
+
+    public void onBtnAction(ActionEvent actionEvent){
+        System.out.println("Kliknięcie nr " + ++clickCounter);
+        if(clickCounter == 1){
+            btnMain.setText("I jeszcze raz");
+        }
+    }
 }
